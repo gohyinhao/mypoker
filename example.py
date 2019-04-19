238ecf89178d14fdb631e7cbc4215823b378c789
gohyinhao <yinhao.g@gmail.com>
2019-04-19 17:31:25 +0800

    Update example parameters

--- example.py
+++ example.py
@@ -4,10 +4,11 @@ from raise_player import RaisedPlayer
 from minimax_player import MinimaxPlayer
 
 # TODO:config the config as our wish
-config = setup_config(max_round=10, initial_stack=10000, small_blind_amount=10)
+config = setup_config(max_round=10, initial_stack=10000, small_blind_amount=20)
 
 
-config.register_player(name="f1", algorithm=MinimaxPlayer([1, 1, 2, 1.5, 20]))
+config.register_player(name="f1", algorithm=MinimaxPlayer(
+    [0.5, 0.5, 0.5, 0.5, 1, 0.5]))
 config.register_player(name="FT2", algorithm=RaisedPlayer())
 
 
